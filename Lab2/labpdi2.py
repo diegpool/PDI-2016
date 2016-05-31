e83ad880d6e473928e8d599e00cfebbacb03beab
diegpool <dieramirez@udec.cl>
2016-05-31 14:54:25 +0000

    actualizacion final lab2 + lab3 inicial

--- Lab2/labpdi2.py
+++ Lab2/labpdi2.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from SimpleCV import*                   # Importamos las librerias necesarias
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
@@ -17,6 +18,7 @@ print "Opciones de papel:"              # Menú de selección de tipo de papel.
 print "1 Blanco"
 print "2 Cuadriculado"
 print "3 Color"
+print "4 Salir"
 
 while True:
     opcion=raw_input("Ingrese opcion: ")
@@ -33,20 +35,26 @@ while True:
         imgBin.save('Fotos/Blanco/Foto binarizada.png')
         imgBininv = imgBin.invert()                 # Se invierte la foto binarazada, para mostrar los resultados
         imgBininv.save('Fotos/Blanco/Foto binarizadaInvertida.png')
+        print "Desea continuar y usar k-means?"
+        sn = raw_input("s/n")
 
-        ##Segmentacion Kmeans
-        image = cv2.imread("Fotos/FotoNormal.png")  # Se carga la imagen a trabajar
-        (largo,ancho)=image.shape[:2]               # Se guardan las dimensiones de la imagen
-        image = image.reshape((image.shape[0] * image.shape[1], 3)) # Se arregla la imagen para poder aplicarle el kmenas
-        clt = KMeans(n_clusters = 2)                # aplicamos kmeans con 2 clusters
-        limites= clt.fit_predict(image)             # guardamos limites de segmentacion
-        quant=clt.cluster_centers_.astype("uint8")[limites]     # cuantizamos la imagen
-        quant=quant.reshape(largo,ancho,3)          # volvemos al tamaño de la imagen original
-        plt.figure()                                # ploteamos la figura resultante
-        plt.axis("off")
-        plt.imshow(quant)
-        plt.show()
-        break 
+        if sn=="s":
+            ##Segmentacion Kmeans
+            image = cv2.imread("Fotos/FotoNormal.png")  # Se carga la imagen a trabajar
+            (largo,ancho)=image.shape[:2]               # Se guardan las dimensiones de la imagen
+            image = image.reshape((image.shape[0] * image.shape[1], 3)) # Se arregla la imagen para poder aplicarle el kmenas
+            clt = KMeans(n_clusters = 2)                # aplicamos kmeans con 2 clusters
+            limites= clt.fit_predict(image)             # guardamos limites de segmentacion
+            quant=clt.cluster_centers_.astype("uint8")[limites]     # cuantizamos la imagen
+            quant=quant.reshape(largo,ancho,3)          # volvemos al tamaño de la imagen original
+            plt.figure(2)                                # ploteamos la figura resultante
+            plt.imshow(quant)
+            plt.show()
+            plt.axis("off")
+            plt.imsave("Fotos/Blanco/Kmeans.png")
+            break
+        if sn=="n":
+            break
                                                     # El proceso para los siguientes tipos de papel, sigue la misma linea sin 
                                                     #presentar grandes diferencias
     if opcion=="2":
@@ -61,7 +69,7 @@ while True:
         print "Guardando foto binarizada..."
         imgBin = imgGris.binarize(60,255,0,5)
         imgBin.save('Fotos/Cuadriculado/Foto binarizada con cuadriculas.png')
-        imgccuad = imgGris.binarize(90,255,0,5)
+        imgccuad = imgGris.binarize(120,255,0,5)
         imgcuadinv=imgccuad.invert()
         imgBininv = imgBin.invert()
         imgBininv.save('Fotos/Cuadriculado/Foto binarizadaInvertida.png')
@@ -69,20 +77,27 @@ while True:
         imgcuad.save('Fotos/Cuadriculado/cuadriculas.png')
         imgcuadn=imgcuad.invert()
         imgcuadn.save('Fotos/Cuadriculado/lineas.png')
-        ##Segmentacion Kmeans
-        image = cv2.imread("Fotos/FotoNormal.png")
-        (largo,ancho)=image.shape[:2]
-        image = image.reshape((image.shape[0] * image.shape[1], 3))
-        clt = KMeans(n_clusters = 2)
-        limites= clt.fit_predict(image)
-        quant=clt.cluster_centers_.astype("uint8")[limites]
-        quant=quant.reshape(largo,ancho,3)
-        plt.figure()
-        plt.axis("off")
-        plt.imshow(quant)
-        plt.show()
-        break 
-    
+
+        print "Desea continuar y usar k-means?"
+        sn = raw_input("s/n")
+
+        if sn=='s':
+            ##Segmentacion Kmeans
+            image = cv2.imread("Fotos/FotoNormal.png")
+            (largo,ancho)=image.shape[:2]
+            image = image.reshape((image.shape[0] * image.shape[1], 3))
+            clt = KMeans(n_clusters = 2)
+            limites= clt.fit_predict(image)
+            quant=clt.cluster_centers_.astype("uint8")[limites]
+            quant=quant.reshape(largo,ancho,3)
+            plt.figure()
+            plt.axis("off")
+            plt.imshow(quant)
+            plt.show()
+            #plt.save("Fotos/Cuadriculado/Kmeans.png")
+            break
+        if sn=="n":
+            break
 
     if opcion=="3":
         ## Segmentacion manual
@@ -97,43 +112,25 @@ while True:
         imgBin.save('Fotos/Color/Foto binarizada.png')
         imgBininv = imgBin.invert()
         imgBininv.save('Fotos/Color/Foto binarizadaInvertida.png')
-        ##Segmentacion Kmeans
-        image = cv2.imread("Fotos/FotoNormal.png")
-        (largo,ancho)=image.shape[:2]
-        image = image.reshape((image.shape[0] * image.shape[1], 3))
-        clt = KMeans(n_clusters = 2)
-        limites= clt.fit_predict(image)
-        quant=clt.cluster_centers_.astype("uint8")[limites]
-        quant=quant.reshape(largo,ancho,3)
-        plt.figure()
-        plt.axis("off")
-        plt.imshow(quant)
-        plt.show()
+        print "Desea continuar y usar k-means?"
+        sn = raw_input("s/n")
+
+        if sn=="s":
+            ##Segmentacion Kmeans
+            image = cv2.imread("Fotos/FotoNormal.png")
+            (largo,ancho)=image.shape[:2]
+            image = image.reshape((image.shape[0] * image.shape[1], 3))
+            clt = KMeans(n_clusters = 2)
+            limites= clt.fit_predict(image)
+            quant=clt.cluster_centers_.astype("uint8")[limites]
+            quant=quant.reshape(largo,ancho,3)
+            plt.figure(2)
+            plt.show(quant)
+            plt.axis("off")
+            plt.show()
+            plt.imsave("Fotos/Color/Kmeans.png")
+            break
+        if sn=="n":
+            break
+    if opcion=="4":
         break
-
-
-##(red,green,blue) = img.splitChannels(False)  
-##print "Guardando foto red..."
-##red.save('Fotos/FotoRed.png')
-##print "Guardando foto blue..."
-##blue.save('Fotos/FotoBlue.png')
-##print "Guardando foto green..."
-##green.save('Fotos/FotoGreen.png')
-##red_histogram = red.histogram(255)
-##blue_histogram = blue.histogram(255)
-##green_histogram = green.histogram(255)
-##plt.figure(2)
-##plt.plot(red_histogram)
-##plt.title('Histograma Escala red')
-##plt.savefig('Hist/HistRed.png')
-##print "Guardando histograma escala red..."
-##plt.figure(3)
-##plt.plot(green_histogram)
-##plt.title('Histograma Escala green')
-##print "Guardando histograma escala green..."
-##plt.savefig('Hist/HistGreen.png')
-##plt.figure(4)
-##plt.plot(blue_histogram)
-##plt.title('Histograma Escala blue')
-##print "Guardando histograma escala blue..."
-##plt.savefig('Hist/HistBlue.png')
