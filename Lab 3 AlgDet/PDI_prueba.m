8c0b025d5986a4acfacd5e979e3b9792d9d48c7c
diegpool <dieramirez@udec.cl>
2016-06-01 12:32:57 -0400

    Actualizacion 1 de junio , medio dia

--- Lab 3 AlgDet/PDI_prueba.m
+++ Lab 3 AlgDet/PDI_prueba.m
@@ -1,33 +1,27 @@
 clear all ; close all; clc;
-img = double(imread('prueba5.jpg'))./255;
 
-red(1,1,1)= 237/255; red(1,1,2) = 28/255; red(1,1,3) = 36/255;
-green(1,1,1)= 34/255; green(1,1,2) = 177/255; green(1,1,3) = 76/255; 
+img = double(imread('Foto.png'))./255;
 
-d = 600;
+red(1,1,1)= 70/255; red(1,1,2) = 32/255; red(1,1,3) = 32/255;
+%green(1,1,1)= 101/255; green(1,1,2) = 97/255; green(1,1,3) = 97/255; 
+
+d1 = 480;
+d2 = 640;
 
 A = zeros(1,1,3);
 B = A;
 numpixA = 0;
 numpixB = 0;
-tolRho = 0.7;
-maximo1 = inf ;
-maximo2 = maximo1;
-for i=1:d
-    for j = 1:d
+tolRho = 0.37;
+
+for i=1:d1
+    for j = 1:d2
         temp = img(i,j,:);
         if( norm3D(temp - red) <= tolRho )
             A = A + temp;
-            if(norm3D(temp-red)<maximo1)
-                maximo1=norm3D(temp-red);
-            end
             numpixA = numpixA + 1;
-        elseif( norm3D(temp - green) <= tolRho )
+        else
             B = B+temp;
-            
-            if(norm3D(temp-green)<maximo2)
-                maximo2=norm3D(temp-green);
-            end
             numpixB = numpixB +1;
         end
     end
@@ -36,14 +30,14 @@ end
 A = A/numpixA;
 B = B/numpixB;
 rho = calRho(A,B);
-%%
+
 imges=[img(:,1,:) img img(:,end,:)];
 imges=[imges(1,:,:); imges ;imges(end,:,:)];
-imgBorde=zeros(d,d,3);
+imgBorde=zeros(d1,d2,3);
 tolBorde = 1;
 
-for i=1:d-1;
-    for j=1:d;
+for i=1:d1-1;
+    for j=1:d2;
     
         Atemp = imges(i+1,j,:);
         Btemp = imges(i+1,j+2,:);
@@ -68,7 +62,7 @@ for i=1:d-1;
     
 end
 
+
 figure;
-imshow(img);
-figure;
-imshow(imgBorde);
+imshow(img+imgBorde(1:480,1:640,:))
+
