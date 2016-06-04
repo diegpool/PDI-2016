151e126a348ffc90e46887fa3c6495ffa01ebce0
diegpool <dieramirez@udec.cl>
2016-06-04 16:40:38 -0400

    Definiendo dimensiones de la imagen a tratar

--- Lab 3 AlgDet/AlgoritmoDetector.py
+++ Lab 3 AlgDet/AlgoritmoDetector.py
@@ -6,17 +6,21 @@ import cv2
 import time
 from Calculo_Rho import rhoCalc
 
-h = 320
-w = 240
+
 c = Camera()
 img = c.live()
 time.sleep(3)
+img = c.getImage()
 
+matImg = img.getNumpy()
+d = matImg.shape
+matBd = np.zeros((d[0] , d[1], 3))
+colorBd = np.array((255,255,255))
 
-d = img.size()
-matImg = img.getNumpy().astype(dtype = 'float64')
+A=np.array([86,53,36])
+B=np.array([133,119,63])
+rho = rhoCalc(A,B);
 
-imgBd = np.zeros((d[0] , d[1], 3))
 tolBd = 1
 
 for i in range(img.width-2):
@@ -26,9 +30,9 @@ for i in range(img.width-2):
 		rhot1 = rhoCalc(At,Bt)
 		rhot2 = rhoCalc(Bt,At)
 		if distRhot(rho,rhot1)<tolBd :
-			imgBd[i+1][j+1] = 0
+			matBd[i+1][j+1] = 0
 		elif distRhot(rho,rhot2)<tolBd :
-			imgBd[i+1][j+1] = 0
+			matBd[i+1][j+1] = 0
 
 
         At = matImg[i][j+1]
@@ -36,9 +40,9 @@ for i in range(img.width-2):
         rhot1 = rhoCalc(At,Bt)
         rhot2=rhoCalc(Bt,At)
         if distRhot(rho,rhot1)<tolBd:
-            imgBd[i+1][j+1]=[255 , 255 ,255]
+            matBd[i+1][j+1]=[255 , 255 ,255]
         elif distRhot(rho,rhot2)<tolBd:
-            imgBd[i+1][j+1]=[255 , 255 ,255]
+            matBd[i+1][j+1]=[255 , 255 ,255]
         
         
         At = matImg[i][j]
@@ -46,9 +50,9 @@ for i in range(img.width-2):
         rhot1 = rhoCalc(At,Bt)
         rhot2 = rhoCalc(Bt,At)
         if distRhot(rho,rhot1)<tolBd :
-            imgBd[i+1][j+1]=[255 , 255 ,255]
+            matBd[i+1][j+1]=[255 , 255 ,255]
         elif distRhot(rho,rhot2)<tolBd :
-            imgBd[i+1][j+1]=[255 , 255 ,255]
+            matBd[i+1][j+1]=[255 , 255 ,255]
         
         
         At = matImg[i][j+2]
@@ -56,18 +60,18 @@ for i in range(img.width-2):
         rhot1 = rhoCalc(At,Bt)
         rhot2 = rhoCalc(Bt,At)
         if distRhot(rho,rhot1)<tolBd :
-            imgBd[i+1][j+1]=[255 , 255 ,255]
+            matBd[i+1][j+1]=[255 , 255 ,255]
         elif distRhot(rho,rhot2)<tolBd :
-            imgBd[i+1][j+1]=[255 , 255 ,255]
+            matBd[i+1][j+1]=[255 , 255 ,255]
         
 
 
-img.show()
 
-imgBd = imgBd.reshape(d[0],d[1],3)
-plt.figure(2)                     
-plt.imshow(imgBd)
-plt.show()
-plt.axis("off")
+
+imgBd = Image(matBd)
+ImgFinal = img+imgBd
+Show = ImgFinal.show()
+
+
 
 time.sleep(100)
