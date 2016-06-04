b7155d3d36cd66e32c0e4537d6e8b143af651d55
diegpool <dieramirez@udec.cl>
2016-06-04 17:10:35 -0400

    correcciones

--- Lab 3 AlgDet/AlgoritmoDetector.py
+++ Lab 3 AlgDet/AlgoritmoDetector.py
@@ -7,14 +7,17 @@ import time
 from Calculo_Rho import rhoCalc
 
 
+
 c = Camera()
 img = c.live()
+print 'Permanezca quieto por favor...'
 time.sleep(3)
 img = c.getImage()
+print 'Imagen capturada !'
 
 matImg = img.getNumpy()
 d = matImg.shape
-matBd = np.zeros((d[0] , d[1], 3))
+matBd = np.zeros_like(matImg)
 colorBd = np.array((255,255,255))
 
 A=np.array([86,53,36])
@@ -22,56 +25,54 @@ B=np.array([133,119,63])
 rho = rhoCalc(A,B);
 
 tolBd = 1
-
+print 'Detectando pixeles de borde...'
+pt = '.'
 for i in range(img.width-2):
 	for j in range(img.height-2):
-		At = matImg[i+1][j+1]
-		Bt = matImg[i+1][j+2]
+
+		At = matImg[i][j+1]
+		Bt = matImg[i+2][j+1]
 		rhot1 = rhoCalc(At,Bt)
 		rhot2 = rhoCalc(Bt,At)
-		if distRhot(rho,rhot1)<tolBd :
-			matBd[i+1][j+1] = 0
-		elif distRhot(rho,rhot2)<tolBd :
-			matBd[i+1][j+1] = 0
+		if np.linalg.norm(rho - rhot1)<tolBd :
+			matBd[i+1][j+1] = colorBd
+		elif np.linalg.norm(rho - rhot2)<tolBd :
+			matBd[i+1][j+1] = colorBd
 
 
-        At = matImg[i][j+1]
-        Bt = matImg[i+2][j+1]
+        At = matImg[i][j]
+        Bt = matImg[i+1][j+2]
         rhot1 = rhoCalc(At,Bt)
         rhot2=rhoCalc(Bt,At)
-        if distRhot(rho,rhot1)<tolBd:
-            matBd[i+1][j+1]=[255 , 255 ,255]
-        elif distRhot(rho,rhot2)<tolBd:
-            matBd[i+1][j+1]=[255 , 255 ,255]
+        if np.linalg.norm(rho - rhot1)<tolBd:
+            matBd[i+1][j+1]=colorBd
+        elif np.linalg.norm(rho - rhot2)<tolBd:
+            matBd[i+1][j+1]=colorBd
         
         
         At = matImg[i][j]
         Bt = matImg[i+2][j+2]
         rhot1 = rhoCalc(At,Bt)
         rhot2 = rhoCalc(Bt,At)
-        if distRhot(rho,rhot1)<tolBd :
-            matBd[i+1][j+1]=[255 , 255 ,255]
-        elif distRhot(rho,rhot2)<tolBd :
-            matBd[i+1][j+1]=[255 , 255 ,255]
-        
+        if np.linalg.norm(rho - rhot1)<tolBd :
+            matBd[i+1][j+1]=colorBd
+        elif np.linalg.norm(rho - rhot2)<tolBd :
+            matBd[i+1][j+1]=colorBd
         
         At = matImg[i][j+2]
         Bt = matImg[i+2][j]
         rhot1 = rhoCalc(At,Bt)
         rhot2 = rhoCalc(Bt,At)
-        if distRhot(rho,rhot1)<tolBd :
-            matBd[i+1][j+1]=[255 , 255 ,255]
-        elif distRhot(rho,rhot2)<tolBd :
-            matBd[i+1][j+1]=[255 , 255 ,255]
+        if np.linalg.norm(rho - rhot1)<tolBd :
+            matBd[i+1][j+1]=colorBd
+        elif np.linalg.norm(rho - rhot2)<tolBd :
+            matBd[i+1][j+1]=colorBd
         
 
 
 
-
+print 'Escaneo completado!'
 imgBd = Image(matBd)
 ImgFinal = img+imgBd
 Show = ImgFinal.show()
-
-
-
 time.sleep(100)
